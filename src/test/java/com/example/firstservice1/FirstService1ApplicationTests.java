57296ddc22520a2e3bfa68b276701dbb0ff75578
doobu-L <seop00513@gmail.com>
2022-08-28 22:46:45 +0900

    Initial commit

---
+++ src/test/java/com/example/firstservice1/FirstService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
